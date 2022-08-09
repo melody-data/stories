7ad26ff73211d222a8b3790fee6507467ae757e5
giuliarenda <giuliarenda94@gmail.com>
2022-08-09 11:54:34 +0200

    new stories automatically added to the index list #2

---
+++ script.js
@@ -0,0 +1,76 @@
+const myData = [];
+let lastFetch = '';
+
+
+window.onload = function () {
+    // check timer
+    const today = createTimer();
+    if (localStorage.getItem('timer') !== today) {
+        fetchData(myData, lastFetch);
+        console.log('fetch');
+    } else {
+        // get the string from localStorage
+        const str = localStorage.getItem('myData');
+        // convert string to valid object
+        const parsedData = JSON.parse(str);
+        updateList(parsedData);
+        console.log('update');
+    }
+}
+
+// fetch data through api call to Githubs
+async function fetchData(myData, lastFetch) {
+    try {
+        const response = await fetch('https://api.github.com/repos/melody-data/stories/contents/published_stories');
+        const data = await response.json();
+        for (obj in data) {
+            let article = data[obj];
+            if (article.name.includes('.json')) {
+                let getJson = await fetch(article.download_url); // fetch the url for raw json
+                let config = await getJson.json();
+                let title = config.title;
+                let file_name = title.replace(/[^\w]/g, '_').toLowerCase() + '_' + config.id + '.html';
+                let path = 'published_stories/' + file_name;
+                myData.push({ title: title, path: path });
+            }
+        }
+        // store data in localStorage
+        localStorage.setItem('myData', JSON.stringify(myData));
+        // update in var
+        updateList(myData);
+        // update timer
+        lastFetch = createTimer();
+        // store timer
+        localStorage.setItem('timer', lastFetch);
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+
+}
+
+
+const createTimer = () => {
+    const date = new Date();
+    return date.toLocaleDateString();
+}
+
+// update list with new data
+const updateList = (data) => {
+    const list = document.getElementById('story_list');
+    for (story of data) {
+        let title = story.title;
+        let li = newListElement(title, story.path);
+        list.appendChild(li);
+    }
+}
+
+// create the list item for each story title
+const newListElement = (title, link) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const aContent = document.createTextNode(title);
+    a.setAttribute('href', link);
+    a.appendChild(aContent);
+    li.appendChild(a);
+    return li;
+}
